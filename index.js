66be50c8056f49b6dd9e4e8727b23223dede7ba5
philipmo <philipmo59@gmail.com>
2023-02-13 20:43:48 -0500

    added loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(names) {
+  let cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return cards;
+};
+
+function countDown(){
+  let i = 11
+  let counter = 0
+  while (counter < 11) {
+    console.log(i-=1)
+    counter += 1
+  }
+}
